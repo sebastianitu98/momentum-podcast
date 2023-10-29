b375d04f1452885fc65257e3e61c76186cb928e8
Itu <sebastianitu61@yahoo.com>
2023-10-29 21:03:40 +0200

    Made changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import FormComponent from './Components/Form/FormComponent'
 import LatestEpisodes from './Components/LatestEpisodes/LatestEpisodes'
 import UpcomingEvents from './Components/UpcomingEvents/UpcomingEvents'
 import CopyrightFooter from './Components/CopyrightFooter/CopyrightFooter'
-import ReteleSocializare from './Components/ReteleSocializare/Retelesocializare'
+import ReteleSocializare from './Components/ReteleSocializare/ReteleSocializare'
 
 
 function App() {
